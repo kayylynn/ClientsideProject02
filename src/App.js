badae23d3ca8d1da7534e2f3be0c7d694a6704c5
kayylynn <kkeene1@stumail.northeaststate.edu>
2023-11-15 12:01:54 -0500

    I had to delete the project and rebuild, I can't remeber it I have already commited this file yet

--- src/App.js
+++ src/App.js
@@ -14,19 +14,25 @@ import React, { useState } from 'react';
     const [humidity, setHumidity] = useState('');
     const [wind_speed, setWindSpeed] = useState('');
 
-    function fetchWeatherHandler(event){
+      function fetchWeatherHandler(event){
       const selectedZip = event.target.value;
 
+      /* THIS CAUSED A FLICKER EACH TIME A NEW CITY WAS SELECTED
+      - it was added so that you did not see the weather section until a city was selected
+      - I did not update the above code so that could have been the problem (ex: const [selectedCity, setSelectedCity] = useState('');
+ )
       setWeatherIcon(null);
       setSelectedCity(null);
       setTemperatureFahrenheit(null);
       setConditions(null);
       setHumidity(null);
       setWindSpeed(null);
+      */
 
       fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${selectedZip}&appid=b9db673980eaac7fbd2bb17f5c7aa904`)
       .then(response => response.json())
       .then(data => {
+
         const temperatureInFahrenheit = ((data.main.temp - 273.15) * 9/5) + 32;
         
         setWeatherIcon(data.weather[0].icon);
@@ -36,18 +42,16 @@ import React, { useState } from 'react';
         setConditions(data.weather[0].description);
         setHumidity(data.main.humidity);
         setWindSpeed(data.wind.speed);
-      })
-  }
+      });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <Header/>
         <img src={logo} className="App-logo" alt="logo" />
-        
         <label htmlFor="cities">Select a City Below:</label>
-
-        <select className="city-select" onChange={fetchWeatherHandler} name="cities" id="cities" value="">
+        <select className="city-select" onChange={fetchWeatherHandler} name="cities" id="cities" value={selectedCity}>
           <option value="" disabled>Select a City</option>
           <option value="98101">Seattle, WA</option>
           <option value="10001">New York, NY</option>
@@ -75,6 +79,6 @@ import React, { useState } from 'react';
     </div>
     
   );
-}
+};
 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,17 +7,13 @@ const Weather = ({ weatherIcon, city, temperature, conditions, humidity, wind_sp
   return (
     <div>
       <p>{`Selected City: ${city}`}</p>
-      {iconUrl && <img src={iconUrl} alt="Weather Icon" />}
-      {temperature !== null && (
-        <div>
-          <span>{`Temperature: ${Math.round(temperature)}°F`}</span><br />
-        </div>
-      )}
-      {conditions !== null && <div><span>{`Conditions: ${conditions}`}</span><br /></div>}
-      {humidity !== null && <div><span>{`Humidity: ${humidity}`}</span><br /></div>}
-      {wind_speed !== null && <div><span>{`Wind Speed: ${wind_speed}`}</span><br /></div>}
+      <img src={iconUrl} alt="Weather Icon" />
+      <div><span>{`Temperature: ${Math.round(temperature)}°F`}</span><br /></div>
+      <div><span>{`Conditions: ${conditions}`}</span><br /></div>
+      <div><span>{`Humidity: ${humidity}`}</span><br /></div>
+      <div><span>{`Wind Speed: ${wind_speed}`}</span><br /></div>
     </div>
   );
-}
+};
 
 export default Weather;
